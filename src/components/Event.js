2fe9769bc23eba4ecc095e0fb7e9a19b897cb7cd
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-22 21:50:01 +0530

    Add src/components/Event.js

---
+++ src/components/Event.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Event = () => {
+  return (
+    <div>
+      <h1>Event</h1>
+      {/* Event content goes here */}
+    </div>
+  );
+};
+
+export default Event;
