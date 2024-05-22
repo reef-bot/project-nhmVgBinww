6428caa799f62edc104b4f9e12f60fc669fe9873
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-22 21:50:02 +0530

    Add src/pages/Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      {/* Home page content goes here */}
+    </div>
+  );
+};
+
+export default Home;
